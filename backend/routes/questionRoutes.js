c58df6a4fbc704e70d789b0c58a5716f8613139a
Karishma <147846941+Karishma133@users.noreply.github.com>
2026-08-10 17:21:03 +0530

    modified questionRoutes.js

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -6,17 +6,13 @@ const {
   listQuestions,
   listCompanies,
   createQuestion,
-  toggleBookmark,
-  listBookmarked,
 } = require("../controllers/questionController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/next", protect, getNextQuestion);
 router.get("/companies/list", protect, listCompanies); // must come before "/:id"
-router.get("/bookmarked/list", protect, listBookmarked); // must come before "/:id"
 router.get("/", protect, listQuestions);
 router.get("/:id", protect, getQuestionById);
-router.post("/:id/bookmark", protect, toggleBookmark);
 router.post("/", protect, createQuestion); // in production, restrict this to admin users
 
 module.exports = router;
